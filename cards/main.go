bad96881b93a671759956e843397cd71ba934980
loclghst <loclghst@gmail.com>
2021-03-25 22:27:50 +0530

    create function receiver with custom type

--- cards/main.go
+++ cards/main.go
@@ -1,16 +1,16 @@
 package main
 
-import "fmt"
-
 func main() {
 	// var card string = "Ace of cards"
 	cards := deck{"Ace of Spades", newCard()} // slice of cards
 
 	cards = append(cards, "Queen of Diamonds") // add to the cards collection
 
-	for i, card := range cards { //iterate over the collection
-		fmt.Println(i, card)
-	}
+	// for i, card := range cards { //iterate over the collection
+	// 	fmt.Println(i, card)
+	// }
+
+	cards.print()
 
 }
 
