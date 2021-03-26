d9c327c60ebf989db059d14f569acf36af68751b
loclghst <loclghst@gmail.com>
2021-03-26 20:27:50 +0530

    add relevant comments

--- cards/deck.go
+++ cards/deck.go
@@ -2,8 +2,12 @@ package main
 
 import "fmt"
 
+// custom type declaration
 type deck []string
 
+// function with a receiver
+// d is a variable which points to the working copy of the deck instance
+// make the function available to any variable of type deck
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
--- cards/main.go
+++ cards/main.go
@@ -1,12 +1,25 @@
 package main
 
 func main() {
-	// var card string = "Ace of cards"
+	// var card string = "Ace of spades" // long way of declaring vars
+
+	// we can also write
+
+	// var card = "Ace of spades" // a bit short format, type is inferred by the compiler
+
+	// or
+
+	// card := "Ace of spades" // declare and initialise, type is inferred by compiler
+
 	cards := deck{"Ace of Spades", newCard()} // slice of cards
 
+	// append doesn't overwrite the original cards,
+	// it returns a new copy after adding the data
 	cards = append(cards, "Queen of Diamonds") // add to the cards collection
 
-	// for i, card := range cards { //iterate over the collection
+	// iterate over the collection
+	// for i, card := range cards {
+	// 	we use := cause for every element new i, cards are declared and initialised
 	// 	fmt.Println(i, card)
 	// }
 
