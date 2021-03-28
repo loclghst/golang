48232b42d7edde0966657b395b9ba4c97a211884
loclghst <loclghst@gmail.com>
2021-03-28 20:54:04 +0530

    test to verify saveDeckToFile and newDeckFromFile

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -8,4 +11,30 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 16 {
 		t.Errorf("Expected deck of length 16 but instead got %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card to be Ace of Spades but instead got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Jack of Hearts" {
+		t.Errorf("Expected last card to be Jack of Hearts but instead got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	// deletes a file or folder of given name
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected deck of length 16 but instead got %v", len(loadedDeck))
+	}
+	// clean up
+	os.Remove("_decktesting")
+
 }
